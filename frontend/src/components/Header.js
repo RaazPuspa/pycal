97883b1f5e187da08799c0d5bc0426e72c89db17
Pusparaj <pusparajbhattarai@yahoo.com>
2020-02-15 19:37:15 +0545

    Add application header as a React component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <div className="app-header-container">
+        <p className="app-title">pyCal</p>
+        <p className="app-sub-title">
+          the Python django + ReactJS calculator
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
